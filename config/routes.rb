b0dc58cf8066f3466e4ce516997be620717d09e2
qdlin <qdlin1314@gmail.com>
2017-04-20 14:01:36 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end 
 
    namespace :admin do
-     resources :jobs do 
+     resources :jobs do
       member do
         post :publish
         post :hide
