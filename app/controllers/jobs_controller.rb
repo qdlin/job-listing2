b8ec382eb03952f3820ac1c24058d40048249a17
qdlin <qdlin1314@gmail.com>
2017-04-28 21:02:39 +0800

    implement search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :validate_search_key, only: [:search]
+
   def show
     @job = Job.find(params[:id])
 
@@ -16,7 +18,7 @@ class JobsController < ApplicationController
             when 'by_upper_bound'
               Job.published.order('wage_upper_bound DESC')
             else
-              Job.published.recent 
+              Job.published.recent
             end
   end
 
@@ -52,6 +54,27 @@ class JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
   end
+
+  def search
+    if @query_string.present?
+      search_result = Job.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+
+  private
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
